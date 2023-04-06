b0aa86c394f132f0478785edf8f888e95714b7a2
filipebratti <filipebratti@hotmail.com>
2023-04-06 20:43:24 -0300

    commit 06.04.2023-2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ export const App = () => {
       <Header />
       <main className="container mt-4">
         <Outlet />
+        <h1>Bem Vindo ao nosso site de Lista de Tarefas!</h1>
+        <p>Clique no menu acima em Menu e assim você poderá adcionar tarefas para realizar.</p>
       </main>
     </div>
   );
